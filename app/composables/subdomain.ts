f49bd746952e458dc8f9b53d49077ed3a609e503
jalil <jalil@1001.digital>
2024-08-19 13:49:43 +0200

    Refactor artist id scope

--- app/composables/subdomain.ts
+++ app/composables/subdomain.ts
@@ -1,25 +1,13 @@
-import type { WatchStopHandle } from 'vue'
-
 export const useSubdomain = () => useState<string|null>('subdomain', () => null)
 
-let idWatcher: WatchStopHandle|null = null
-const id: Ref<`0x${string}`|null> = ref(null)
 export const useArtistId = () => {
   const route = useRoute()
   const subdomain = useSubdomain()
 
-  const updateId = () => {
-    id.value = subdomain.value
-      ? subdomain.value as `0x${string}`
-      : route.params.id
-        ? (route.params.id as string).toLowerCase() as `0x${string}`
-        : null
-  }
-
-  if (! idWatcher) {
-    updateId()
-    idWatcher = watch(() => route.params.id, () => updateId())
-  }
-
-  return id
+  return computed(() => subdomain.value
+    ? subdomain.value as `0x${string}`
+    : route.params.id
+      ? (route.params.id as string).toLowerCase() as `0x${string}`
+      : null
+  )
 }
