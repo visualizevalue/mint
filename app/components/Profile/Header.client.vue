9a3d4b63cbf45f419f4961bce4ef5cf6dd1e92d1
jalil <jalil@1001.digital>
2024-08-19 19:38:40 +0200

    Style header section

--- app/components/Profile/Header.client.vue
+++ app/components/Profile/Header.client.vue
@@ -1,13 +1,18 @@
 <template>
   <header>
-    <img v-if="artist?.avatar" :src="artist.avatar" :alt="artist.ens">
-    <h1 v-if="artist?.ens">{{ artist.ens }} <small>{{ artist.address }}</small></h1>
-    <p v-else>{{ address }}</p>
+    <img :src="artist.avatar || `/icons/opepen.svg`" :alt="name">
+    <h1 @click="() => copy(address)">
+      <span>{{ name }}</span>
+      <small v-if="copied">copied...</small>
+      <small v-else-if="artist.ens">{{ shortAddress(address) }}</small>
+    </h1>
     <p v-if="artist?.description">{{ artist.description }}</p>
   </header>
 </template>
 
 <script setup>
+import { useClipboard } from '@vueuse/core'
+
 const props = defineProps({
   address: String,
 })
@@ -15,4 +20,40 @@ const props = defineProps({
 const store = useOnchainStore()
 
 const artist = computed(() => store.artist(props.address))
+const name = computed(() => artist.value?.ens || shortAddress(props.address))
+const artistAddress = computed(() => props.address)
+const { copy, copied } = useClipboard({ source: artistAddress })
 </script>
+
+<style lang="postcss" scoped>
+header {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: var(--spacer);
+
+  img {
+    width: var(--size-9);
+    height: var(--size-9);
+    border-radius: 50%;
+    border: var(--border);
+  }
+
+  h1 {
+    cursor: pointer;
+
+    > * {
+      display: block;
+    }
+
+    small {
+      color: var(--muted);
+    }
+  }
+
+  p {
+    color: var(--muted);
+  }
+}
+</style>
