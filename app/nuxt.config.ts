2c7e6a537999ad4a26dbed559ce8820269e4be55
jalil <jalil@1001.digital>
2024-08-23 22:02:35 +0200

    Change env name

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -3,7 +3,7 @@
 export default defineNuxtConfig({
   // devtools: { enabled: true },
 
-  ssr: process.env.STATIC_SITE !== 'true',
+  ssr: process.env.NUXT_SSR !== 'false',
 
   runtimeConfig: {
     public: {
