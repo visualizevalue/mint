40d92607be1707423c9b25c1e6d114dfe0986662
jalil <jalil@1001.digital>
2024-08-24 09:28:25 +0200

    Add logging

--- app/server/middleware/subdomain.ts
+++ app/server/middleware/subdomain.ts
@@ -4,6 +4,8 @@ export default defineEventHandler(({ node, context }) => {
   const sub = hostname.split('.')[0]?.toLowerCase() as string
   const main = process.env.NUXT_PUBLIC_DOMAIN?.split('.')[0] || `localhost`
 
+  console.log('sub, main', sub, main)
+
   if (sub !== main) {
     context.subdomain = sub
   }
