33b5773ded4f6813695ac4b44aed359768d84751
jalil <jalil@1001.digital>
2024-08-29 16:35:39 +0200

    Update sepolia deployment

--- contracts/tasks/export-abis.ts
+++ contracts/tasks/export-abis.ts
@@ -2,11 +2,11 @@ import { zeroAddress } from 'viem'
 import { formatAbi } from 'abitype'
 import { task } from 'hardhat/config'
 
-task('export:abi:factory', 'Exports an abi in its human readable form')
+task('export:abi:factory:v1', 'Exports an abi in its human readable form')
   .setAction(async (_, hre) => {
     const contractMetadata = await hre.viem.deployContract('ContractMetadata', [])
 
-    const factory = await hre.viem.deployContract('Factory', [zeroAddress, zeroAddress], {
+    const factory = await hre.viem.deployContract('FactoryV1', [], {
       libraries: {
         ContractMetadata: contractMetadata.address,
       }
