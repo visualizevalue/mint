fdf9f307e68ae525a9e96b4482e8b3697a293f2a
jalil <jalil@1001.digital>
2024-09-02 18:18:31 +0200

    Copy changes for profiles

--- app/pages/profile/[address]/index.vue
+++ app/pages/profile/[address]/index.vue
@@ -13,8 +13,13 @@
       </template>
     </ProfileHeader>
 
-    <section class="centered">
-      <p class="muted">Indexing collected art will come soon...</p>
+    <section v-if="artistScope" class="centered muted">
+      <p>Collected art from <Account :address="artistScope" /> will be indexed on profiles soon...</p>
+      <p v-if="isMyArtistScope">Manage Your collections <NuxtLink :to="{ name: 'id', params: { id: address } }">here</NuxtLink>.</p>
+      <p v-else>You can mint your own art on <NuxtLink :to="config.public.platformUrl" target="_blank">{{ getMainDomain(config.public.platformUrl) }}</NuxtLink>.</p>
+    </section>
+    <section v-else class="centered">
+      <p class="muted">Collected art and curation options for your profile will come soon...</p>
     </section>
   </PageFrame>
 </template>
@@ -25,6 +30,8 @@ const route = useRoute()
 const address = computed(() => route.params.address)
 const store = useOnchainStore()
 const isMe = useIsMeCheck(address.value)
+const artistScope = useArtistScope()
+const isMyArtistScope = useIsMeCheck(artistScope)
 
 const artist = ref(null)
 
