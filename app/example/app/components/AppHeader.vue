0bce23f97005f69c155f38044dbf4fd3f61f836f
jalil <jalil@1001.digital>
2024-09-10 20:41:29 +0200

    Simplify example header

--- app/example/app/components/AppHeader.vue
+++ app/example/app/components/AppHeader.vue
@@ -11,15 +11,5 @@ import HeaderBase from '@visualizevalue/mint-app-base/app/components/AppHeader'
     background: var(--gray-z-1);
     border-color: var(--gray-z-3) !important;
   }
-
-  .lightmode {
-    header.custom-header {
-      --color: var(--gray-z-9);
-      --muted: var(--gray-z-8);
-      --muted-light: var(--gray-z-6);
-
-      background: var(--gray-z-4);
-    }
-  }
 </style>
 
