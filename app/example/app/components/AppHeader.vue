0b9ddb1f973af78c4cfd974e237f5f2a31d31958
jalil <jalil@1001.digital>
2024-09-17 14:24:27 +0200

    Style fixes

--- app/example/app/components/AppHeader.vue
+++ app/example/app/components/AppHeader.vue
@@ -13,12 +13,13 @@ const { y } = useWindowScroll()
 </script>
 
 <style scoped>
-  header {
+  .app-header {
     position: sticky;
     top: 0;
     z-index: 100;
     background: var(--background-semi);
     backdrop-filter: var(--blur);
+    padding: var(--ui-padding-y) var(--ui-padding-x) !important;
 
     border-bottom: var(--border);
     border-color: transparent;
