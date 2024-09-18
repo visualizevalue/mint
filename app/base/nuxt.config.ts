132f67bd0d93ebf30b8fc45e9b9d64175f5d5c57
jalil <jalil@1001.digital>
2024-09-18 02:21:29 +0200

    Remove default devtools

--- app/base/nuxt.config.ts
+++ app/base/nuxt.config.ts
@@ -5,7 +5,6 @@ const currentDir = dirname(fileURLToPath(import.meta.url))
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
   ssr: process.env.NUXT_SSR !== 'false',
 
   runtimeConfig: {
