af83f077ed198359cdf08d7868bb1ba090c0d61a
jalil <jalil@1001.digital>
2024-09-25 15:57:50 +0200

    Start w p5 renderer

---
+++ contracts/contracts/renderers/P5Renderer.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import { Strings   } from "@openzeppelin/contracts/utils/Strings.sol";
+import { Base64    } from "@openzeppelin/contracts/utils/Base64.sol";
+import { IRenderer } from "./../interfaces/IRenderer.sol";
+import { SSTORE2   } from "./../libraries/SSTORE2.sol";
+import { Token     } from "./../types/Token.sol";
+
+contract P5Renderer is IRenderer {
+
+
+    function uri (uint tokenId, Token calldata token, bytes memory artifact) external pure returns (string memory) {
+        bytes memory dataURI = abi.encodePacked(
+            '{',
+                '"id": "', Strings.toString(tokenId), '",',
+                '"name": "', token.name, '",',
+                '"description": "', token.description, '",',
+                '"image": "', artifact, '"',
+                '"animation_uri": "', artifact, '"',
+            '}'
+        );
+
+        return string(
+            abi.encodePacked(
+                "data:application/json;base64,",
+                Base64.encode(dataURI)
+            )
+        );
+    }
+
+}
