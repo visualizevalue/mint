6183ea13a325ee248654189922c88bc2c2ec1d28
jalil <jalil@1001.digital>
2024-09-27 18:32:45 +0200

    Add chain config for contract verification

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -77,6 +77,23 @@ const config: HardhatUserConfig = {
       },
     },
   },
+  etherscan: {
+    apiKey: {
+      mainnet: process.env.ETHERSCAN_API_KEY as string,
+      sepolia: process.env.ETHERSCAN_API_KEY as string,
+      holesky: process.env.ETHERSCAN_API_KEY as string,
+    },
+    customChains: [
+      {
+        network: 'holesky',
+        chainId: 17000,
+        urls: {
+          apiURL: 'https://api-holesky.etherscan.io/api',
+          browserURL: 'https://holesky.etherscan.io'
+        }
+      }
+    ],
+  },
   mocha: {
     timeout: 120_000,
   },
