7be4edb28801a04a7b855551d5b163685afb354e
jalil <jalil@1001.digital>
2024-11-12 03:28:08 +0100

    Deploy p5 renderer

--- app/base/app.config.ts
+++ app/base/app.config.ts
@@ -4,7 +4,7 @@ export default defineAppConfig({
       component: 'P5',
       name: 'P5 Renderer',
       version: 1n,
-      address: '0xe7bbcb8aaa9fd0acb720c24b4c44fad521ba621f',
+      address: '0x32B8Ffa14e7F77c252b6D43BEC5498FCef2b205F',
       description: 'Allows using P5 scripts as the artifact content'
     },
   ],
