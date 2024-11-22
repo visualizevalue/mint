581cd501e84643ce3fa6d315d0b420ad8dbc7a28
jalil <jalil@1001.digital>
2024-11-23 00:14:31 +0100

    Configure P5 renderers (v2)

--- app/base/app.config.ts
+++ app/base/app.config.ts
@@ -4,9 +4,16 @@ export default defineAppConfig({
     1: [
       {
         component: 'P5',
-        name: 'P5 Renderer',
+        name: 'P5 Renderer [Deprecated]',
         version: 1n,
         address: '0x32b8ffa14e7f77c252b6d43bec5498fcef2b205f',
+        description: 'Allows using P5 scripts as the artifact content. Deprecated: Please use V2'
+      },
+      {
+        component: 'P5',
+        name: 'P5 Renderer',
+        version: 2n,
+        address: '0xbf42cabb7d46bfa290dcc6223477c6afe6a83174',
         description: 'Allows using P5 scripts as the artifact content'
       },
       {
@@ -21,9 +28,16 @@ export default defineAppConfig({
     11155111: [
       {
         component: 'P5',
-        name: 'P5 Renderer (Sepolia)',
+        name: 'P5 Renderer (Sepolia) [Deprecated]',
         version: 1n,
         address: '0xfadf2fb2f8a15fc830c176b71d2c905e95f4169e',
+        description: 'Allows using P5 scripts as the artifact content. Deprecated: Please use V2'
+      },
+      {
+        component: 'P5',
+        name: 'P5 Renderer (Sepolia)',
+        version: 2n,
+        address: '0x55b69a4f2db99417c1b211151181ed48b39df438',
         description: 'Allows using P5 scripts as the artifact content'
       },
       {
