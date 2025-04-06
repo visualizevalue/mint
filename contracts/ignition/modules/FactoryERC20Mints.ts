4af0ffd72209bb266e22ff4571a0350911acfd15
jalil <jalil@1001.digital>
2025-04-06 22:58:13 +0200

    Deploy with initial mint contract verification config

--- contracts/ignition/modules/FactoryERC20Mints.ts
+++ contracts/ignition/modules/FactoryERC20Mints.ts
@@ -1,10 +1,19 @@
 import { buildModule } from '@nomicfoundation/hardhat-ignition/modules'
+import { toByteArray } from '@visualizevalue/mint-utils'
+import { LOGO, VV } from '../../test/constants'
 
 const FactoryERC20MintsModule = buildModule('FactoryERC20Mints', (m) => {
   // Prepare base contracts
   const contractMetadata = m.contractAt('ContractMetadata', m.getParameter('contractMetadata'))
   const artifactReader = m.contractAt('ArtifactReader', m.getParameter('artifactReader'))
   const renderer = m.contractAt('Renderer', m.getParameter('renderer'))
+
+  const mint = m.contract('MintViaERC20', [], {
+    libraries: {
+      ContractMetadata: contractMetadata,
+    }
+  })
+  m.call(mint, 'init', ['Mint via ERC20', 'MINT20', '', toByteArray(LOGO), renderer, VV])
 
   // Deploy the factory for MintViaERC20 contracts
   const factoryERC20Mints = m.contract('FactoryERC20Mints', [renderer], {
