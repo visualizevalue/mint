11d58f1c2f444ed796343238cf8fe6e42b87eef0
jalil <jalil@1001.digital>
2025-04-08 10:44:17 +0200

    Clear balances on checkout

--- app/base/composables/collections.ts
+++ app/base/composables/collections.ts
@@ -391,6 +391,14 @@ export const useOnchainStore = () => {
         }
       },
 
+      clearTokenBalance (token: Token) {
+        this.tokenBalances[token.collection] = {}
+      },
+
+      clearAllTokenBalances () {
+        this.tokenBalances = {}
+      },
+
       async fetchTokenBalance (token: Token, address: `0x${string}`) {
         const client = getPublicClient($wagmi, { chainId })
         const mintContract = getContract({
@@ -400,7 +408,7 @@ export const useOnchainStore = () => {
         })
 
         if (! this.tokenBalances[token.collection]) {
-          this.tokenBalances[token.collection] = {}
+          this.clearTokenBalance(token)
         }
 
         this.tokenBalances[token.collection][`${token.tokenId}`] =
