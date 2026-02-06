4327dfc3e7a5e348c9d06537b295d5d159487693
jalil <jalil@1001.digital>
2026-02-07 00:05:14 +0100

    Refactor config variables

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -3,6 +3,8 @@ import type { HardhatUserConfig } from "hardhat/config";
 import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
 import { configVariable } from "hardhat/config";
 
+const accountKey = configVariable(process.env.ACCOUNT_KEY || "PRIVATE_KEY");
+
 import { accountsTask, fundAccountTask } from "./tasks/accounts.js";
 import { mineTask, blockTask, purchaseTask } from "./tasks/chain.js";
 import { exportAbiFactoryTask, exportAbiMintTask, exportAbiRendererTask } from "./tasks/export-abis.js";
@@ -61,26 +63,31 @@ const config: HardhatUserConfig = {
       chainType: "l1",
       blockGasLimit: 1_000_000_000,
       forking: {
-        url: configVariable("MAINNET_URL"),
+        url: configVariable("MAINNET_RPC_URL"),
       }
     },
     mainnet: {
       type: "http",
       chainType: "l1",
-      url: configVariable("MAINNET_URL"),
-      accounts: [configVariable("PRIVATE_KEY")],
+      url: configVariable("MAINNET_RPC_URL"),
+      accounts: [accountKey],
     },
     sepolia: {
       type: "http",
       chainType: "l1",
-      url: configVariable("SEPOLIA_URL"),
-      accounts: [configVariable("PRIVATE_KEY")],
+      url: configVariable("SEPOLIA_RPC_URL"),
+      accounts: [accountKey],
     },
     holesky: {
       type: "http",
       chainType: "l1",
-      url: configVariable("HOLESKY_URL"),
-      accounts: [configVariable("PRIVATE_KEY")],
+      url: configVariable("HOLESKY_RPC_URL"),
+      accounts: [accountKey],
+    },
+  },
+  verify: {
+    etherscan: {
+      apiKey: configVariable("ETHERSCAN_API_KEY"),
     },
   },
 };
